348a660809f378c2315932de39242d1693a39015
Emma Parsley <40206111@live.napier.ac.uk>
2018-09-11 02:38:18 +0100

    Flashing line -Added Line that automatically resizes and moves to be with the rest of the font -Added script to make line flash (does not work)

---
+++ Code Game/Assets/Scripts/flashline.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class flashline : MonoBehaviour
+{
+
+    private float time = 0.0f;
+    private Color sr;
+
+    // Use this for initialization
+    void Start()
+    {
+        sr = GetComponent<SpriteRenderer>().color;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time += Time.deltaTime;
+        if (time > 0.5f)
+        {
+            if (sr.a == 255)
+            {
+                sr.a = 0;
+            }
+            else
+            {
+                sr.a = 255;
+            }
+            time = 0.0f;
+        }
+    }
+}
--- Code Game/Assets/Scripts/getInput.cs
+++ Code Game/Assets/Scripts/getInput.cs
@@ -2,17 +2,27 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class getInput : MonoBehaviour {
+public class getInput : MonoBehaviour
+{
 
     public UnityEngine.UI.Text theText;
+    [SerializeField]
+    private GameObject line;
+    private int textPlace = 0;
 
     // Use this for initialization
-    void Start () {
+    void Start()
+    {
         theText = GetComponent<UnityEngine.UI.Text>();
+        line.transform.localScale = new Vector2(theText.fontSize / 8, theText.fontSize);
+        line.GetComponent<SpriteRenderer>().color = theText.color;
+        RectTransform temp = theText.GetComponent<RectTransform>();
+        line.transform.localPosition = new Vector2((temp.localPosition.x - temp.rect.width/2) - theText.fontSize / 3, (temp.localPosition.y + temp.rect.height/2) + theText.fontSize / 4);
     }
-	
-	// Update is called once per frame
-	void Update () {
+
+    // Update is called once per frame
+    void Update()
+    {
         foreach (char c in Input.inputString)
         {
             if (c == '\b') // has backspace/delete been pressed?
