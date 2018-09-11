43a94cfd852349085de70bc96c6cc093840e8d0c
Emma Parsley <40206111@live.napier.ac.uk>
2018-09-11 01:53:33 +0100

    Typing -You can type to the screen, delete and make new lines.

---
+++ Code Game/Assets/Scripts/getInput.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class getInput : MonoBehaviour {
+
+    public UnityEngine.UI.Text theText;
+
+    // Use this for initialization
+    void Start () {
+        theText = GetComponent<UnityEngine.UI.Text>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        foreach (char c in Input.inputString)
+        {
+            if (c == '\b') // has backspace/delete been pressed?
+            {
+                if (theText.text.Length != 0)
+                {
+                    theText.text = theText.text.Substring(0, theText.text.Length - 1);
+                }
+            }
+            else if ((c == '\n') || (c == '\r')) // enter/return
+            {
+                theText.text += '\n';
+            }
+            else
+            {
+                theText.text += c;
+            }
+        }
+    }
+}
