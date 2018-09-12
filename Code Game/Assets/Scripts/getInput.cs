6c2ee47adbae1d38e246623a061da0ca0560963e
40206111 <40206111@live.napier.ac.uk>
2018-09-12 15:22:07 +0100

    Moving Line -Vertical movement implemented

--- Code Game/Assets/Scripts/getInput.cs
+++ Code Game/Assets/Scripts/getInput.cs
@@ -9,6 +9,7 @@ public class getInput : MonoBehaviour
     [SerializeField]
     private GameObject line;
     private int textPlace = 0;
+    private float xVal = 0;
 
     // Use this for initialization
     void Start()
@@ -18,6 +19,7 @@ public class getInput : MonoBehaviour
         line.GetComponent<SpriteRenderer>().color = theText.color;
         RectTransform temp = theText.GetComponent<RectTransform>();
         line.transform.localPosition = new Vector2((temp.localPosition.x - temp.rect.width/2) - theText.fontSize / 3, (temp.localPosition.y + temp.rect.height/2) + theText.fontSize / 4);
+        xVal = line.transform.localPosition.x;
     }
 
     // Update is called once per frame
@@ -29,13 +31,23 @@ public class getInput : MonoBehaviour
             {
                 if (theText.text.Length != 0)
                 {
-                    StartCoroutine(MoveLineLeft(theText.text[theText.text.Length - 1]));
+                    char temp = theText.text[theText.text.Length - 1];
+                    if ((temp == '\n') || (temp == '\r'))
+                    {
+                        line.transform.localPosition = new Vector3(xVal, line.transform.localPosition.y + (theText.fontSize * 1.08f), line.transform.localPosition.z);
+                        lastLine();
+                    }
+                    else
+                    {
+                        StartCoroutine(MoveLineLeft(temp));
+                    }
                     theText.text = theText.text.Substring(0, theText.text.Length - 1);
                 }
             }
             else if ((c == '\n') || (c == '\r')) // enter/return
             {
                 theText.text += '\n';
+                line.transform.localPosition = new Vector3(xVal, line.transform.localPosition.y - (theText.fontSize * 1.08f), line.transform.localPosition.z);
             }
             else
             {
@@ -60,5 +72,16 @@ public class getInput : MonoBehaviour
         CharacterInfo charInfo;
         theText.font.GetCharacterInfo(c, out charInfo);
         line.transform.localPosition = new Vector3(line.transform.localPosition.x - (charInfo.advance * 2.165f), line.transform.localPosition.y, line.transform.localPosition.z);
+    }
+
+    private void lastLine()
+    {
+        char[] newlines = { '\r', '\n' };
+        string[] temp = theText.text.Split(newlines);
+        string theOne = temp[temp.Length - 2];
+        for (int i = 0; i < theOne.Length; i ++)
+        {
+            StartCoroutine(MoveLineRight(theOne[i]));
+        }
     }
 }
