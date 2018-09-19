a3393a2af7743d55cf8312518d708c3bccca3113
NAPIER-MAIL\40206111 <40206111@live.napier.ac.uk>
2018-09-19 12:54:17 +0100

    Scrollbar -Working on getting a working scrollbar

---
+++ Code Game/Assets/Scripts/HorizontalScroll.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HorizontalScroll : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject panel;
+    private RectTransform panRect;
+    [SerializeField]
+    private UnityEngine.UI.InputField inputBox;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+    public void OnValueChanged()
+    {
+        Debug.Log("test");
+    }
+}
