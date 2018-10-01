1b7b6632b207e2b536ac4c583b533f2db0d02d49
Emma Parsley <40206111@live.napier.ac.uk>
2018-10-01 02:40:23 +0100

    Working on Scrolling Behaviour -Think I am closer to working solution but it's not as simple as I'd have thought -Get's Caret's local position

---
+++ Code Game/Assets/Scripts/CaretPos.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class CaretPos : InputField
+{
+    public Vector2 LocalCaretPos()
+    {
+        if (isFocused == true)
+        {
+            TextGenerator gen = m_TextComponent.cachedTextGenerator;
+            UICharInfo charInfo = gen.characters[caretPosition];
+            float x = (charInfo.cursorPos.x + charInfo.charWidth) / m_TextComponent.pixelsPerUnit;
+            float y = (charInfo.cursorPos.y) / m_TextComponent.pixelsPerUnit;
+            return new Vector2(x, y);
+        }
+        else
+        {
+            return new Vector2(0.0f, 0.0f);
+        }
+    }
+}
--- Code Game/Assets/Scripts/HorizontalScroll.cs
+++ Code Game/Assets/Scripts/HorizontalScroll.cs
@@ -1,54 +1,71 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class HorizontalScroll : MonoBehaviour {
+public class HorizontalScroll : MonoBehaviour
+{
 
     [SerializeField]
     private GameObject panel;
     private RectTransform panRect;
     [SerializeField]
-    private UnityEngine.UI.InputField inputBox;
+    private InputField inputBox;
     [SerializeField]
     private GameObject textField;
     private RectTransform textRect;
     [SerializeField]
-    private UnityEngine.UI.Scrollbar sc;
+    private Scrollbar sc;
+    private CaretPos Carrot;
     [SerializeField]
-    private Transform Carrot;
-
+    private Canvas can;
     private float ratio = 1;
     private float prevVal = 0;
 
-	// Use this for initialization
-	void Start () {
+    [SerializeField]
+    private GameObject otherText;
+
+    // Use this for initialization
+    void Start()
+    {
         panRect = panel.GetComponent<RectTransform>();
         textRect = textField.GetComponent<RectTransform>();
-        Carrot = transform.Find("InputField Input Caret");
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        if (Carrot == null)
+        Carrot = GetComponent<CaretPos>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("right"))
         {
-            Carrot = transform.Find("InputField Input Caret");
-        }
-        else
-        {
-            Vector3 pos = Carrot.transform.localPosition;
-            pos.x += 1;
-            Carrot.transform.localPosition = pos;
+            float var1 = Carrot.LocalCaretPos().x;
+            float var2 = otherText.transform.position.x / can.scaleFactor;
+            float var3 = panRect.transform.position.x / can.scaleFactor;
+            float var4 = (panRect.rect.width / 2);
+            Debug.Log("x: " + var1 + ", y: " + var2
+                + ", z: " + var3 + ", w: " + var4);
+            //Values wrong currently
+            if (var1 + var2 > var3 + var4)
+            {
+                Debug.Log("Something significant or distinct");
+                sc.value += 0.25f * ratio;
+            }
         }
     }
 
     public void resizeBar()
     {
-        if (ratio < 1 || textRect.rect.width > (panRect.rect.width-2))
+        if (ratio < 1 || textRect.rect.width > (panRect.rect.width * 0.75))
         {
-            ratio = panRect.rect.width / textRect.rect.width;
+            ratio = (panRect.rect.width * 0.75f) / textRect.rect.width;
             sc.size = ratio;
             Canvas.ForceUpdateCanvases();
         }
+        //Values wrong currently
+        //if (Carrot.LocalCaretPos().x + otherText.transform.position.x > panRect.transform.position.x * can.scaleFactor)
+        //{
+        //    sc.value += 0.25f * ratio;
+        //}
     }
 
     public void OnValueChanged()
@@ -56,7 +73,9 @@ public class HorizontalScroll : MonoBehaviour {
         float diff = prevVal - sc.value;
         prevVal = sc.value;
         Vector3 pos = inputBox.transform.localPosition;
-        pos.x += ((textRect.rect.width - panRect.rect.width/2) * diff);
+        pos.x += ((textRect.rect.width - panRect.rect.width / 2) * diff);
         inputBox.transform.localPosition = pos;
+
     }
+
 }
