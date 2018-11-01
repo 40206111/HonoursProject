c6c452c3ff460e1a856067d09309f9f9ec5a88c3
40206111 <40206111@live.napier.ac.uk>
2018-11-01 17:35:46 +0000

    Zombie Game Start -Started the game the players will code

---
+++ Code Game/Assets/scripts/Zombie.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Zombie : MonoBehaviour {
+
+    private GameObject player;
+    [SerializeField]
+    private float speed = 2.0f;
+
+	// Use this for initialization
+	void Start () {
+        player = GameObject.FindGameObjectWithTag("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 direction = player.transform.localPosition - gameObject.transform.localPosition;
+        direction = direction.normalized;
+        gameObject.transform.localPosition += direction * speed * Time.deltaTime;
+
+	}
+}
