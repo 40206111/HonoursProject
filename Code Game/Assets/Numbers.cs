c1860b91d2367fea74b4d31b4ce71c210a99e0f4
Emma Parsley <40206111@live.napier.ac.uk>
2018-12-03 03:00:18 +0000

    Line numbers -are made but don't dissapear -Don't work with the word wrap

---
+++ Code Game/Assets/Numbers.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Numbers : MonoBehaviour {
+
+    [SerializeField]
+    private TMP_InputField theText;
+    [SerializeField]
+    private TMP_InputField lineNos;
+
+    private static int charicters = 0;
+    private static int current = 1;
+
+    public void OnValueChanged()
+    {
+        int change = theText.text.Length - charicters;
+        charicters = theText.text.Length;
+
+        for (int i = 0; i < change; ++i)
+        {
+            if (theText.text[theText.text.Length - i - 1] == '\n')
+            {
+                current += 1;
+                lineNos.text += "\n" + current;
+            }
+        }
+      }
+}
