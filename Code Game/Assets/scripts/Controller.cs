95e603c98730c7bb869db737205a378d81a8b022
Emma Parsley <40206111@live.napier.ac.uk>
2019-01-09 03:25:30 +0000

    Oops
    -See last commit, I managed to commit everything but the important code file

--- Code Game/Assets/scripts/Controller.cs
+++ Code Game/Assets/scripts/Controller.cs
@@ -5,6 +5,12 @@ using TMPro;
 
 public class Controller : MonoBehaviour
 {
+    [SerializeField]
+    private TMP_InputField numbers;
+    [SerializeField]
+    private TMP_Text theText;
+    private static int lines = 1;
+
 
     // Start is called before the first frame update
     void Start()
@@ -14,6 +20,65 @@ public class Controller : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //Stop MSpace Being Deleted (if input box selected and carat position = 0 and backspace pressed, replace text with monospace tag.)
+        LineNumbers();
+    }
+
+    void LineNumbers()
+    {
+        ///Abiltiy could be added to have text resize if it gets too big
+            ///e.g. if characters has increased
+            ///add <size = x> to text before adding value
+            ///if characters will decrease
+            ///remove 9 + x.tostring.length extra characters
+
+        int current = theText.textInfo.lineCount;
+        while (lines > current)
+        {
+            numbers.text = numbers.text.Substring(0, numbers.text.Length - (lines.ToString().Length + 1));
+            lines--;
+        }
+        while (lines < current)
+        {
+            lines++;
+            numbers.text += lines + "\n";
+        }
+    }
+
+    public void Compile()
+    {
+        ////
+        ///Break text by ;
+        ///until not usings
+        ///Add usings to list
+        ///for all commands after usings
+        ///incriment by 1 every new line and every 50 (dependednt on point size -> 1120/pointsize) characters (counting line numbers)
+        ///If line does not equal using check for class (if not output error on current line)
+        ///check for open bracket (increment bracket count)
+        ///if in class while bracketcount = 1 start class check
+        ///if bracket count is zero check for class or end of file
+        ///if error break loops and continue with error help
+    }
+
+    private bool ClassCheck(int lineNo)
+    {
+        ///if close bracket decrement bracket count
+        ///check for variable store variables
+        ///check for method store methods
+        ///check for open bracket (increment bracket count)
+        ///if in method while bracket count = 2 start method check
+        ///if error return false
+        return false;
+    }
+
+    private bool MethodCheck(int lineNo)
+    {
+        //method class or struct that contains variables
+        //check for variables and store within method
+        //if error retur false
+        return false;
+    }
+
     public void Run()
     {
         ////
