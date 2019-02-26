e37ec35252e0ed9d58286cdbb7d8cf1f61b720dc
40206111 <40206111@live.napier.ac.uk>
2019-02-26 01:23:02 +0000

    Starting
    -Main menu
    -Quit
    -Start
    -New colour scheme

---
+++ Code Game/Assets/scripts/UIMaster.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIMaster : MonoBehaviour
+{
+
+    public void Begin()
+    {
+        SceneManager.LoadScene("Start");
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+
+    public void Menu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+}
